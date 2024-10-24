ff154da502c88eb877695d13e4536b60c7455b8c
laryssascalia <160618340+laryssascalia@users.noreply.github.com>
2024-10-24 11:11:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Desenhos',
+    'ONDE O BOB ESPONJA MORA ?',
+    'EM UM ABACAXI'
+)
+
+criaCartao(
+    'Desenhos',
+    'QUAL É O INIMIGO DO JERRY?',
+    'TOM'
+)
+
+criaCartao(
+    'Desenhos',
+    'QUEM É O MELHOR AMIGO DO KIKI BUTOVISK?',
+    'GUNTHER MAGNUSON'
+)
+
+criaCartao(
+    'Desenhos',
+    'QUEM E O VIZINHO DO PICA-PAU?',
+    'LEÔNCIO'
+)
+criaCartao(
+    'Desenhos',
+    'QUEM QUE SEMPRE TENTA ROUBAR A DORA?',
+    'O RAPOSO'
+)
+criaCartao(
+    'Desenhos',
+    'OSCAR ERA O PRIMEIRO OU A ÚLTIMA FATIA DO PÃO DE FORMA?',
+    'PRIMEIRA'
+)
